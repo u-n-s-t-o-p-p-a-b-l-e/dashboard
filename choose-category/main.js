c56073ce8c12b46a7d082042a32958c0da6d4f52
unstoppable <internalminds01@gmail.com>
2024-02-27 12:31:59 +0800

    JS updated few lines

--- choose-category/main.js
+++ choose-category/main.js
@@ -2,3 +2,32 @@ function selectCategory($category) {
 	$category.toggleClass('category-selected');
 	updateInfobox();
 }
+
+function updateInfobox() {
+	var $selectedCategories = $('.category-selected');
+	var selectedCount = $selectedCategories.length;
+	var selectedCategories = [];
+	$selectedCategories.each(function() {
+		var name = $(this).find('.category-name').text();
+		selectedCategories.push(name);
+	});
+	var totalCategories = $('.category').length;
+	$infoboxCount.text(`${selectedCount} / ${totalCategories} selected`);
+	$infoboxNames.html(selectedCategories.join(' / ') || '&nbsp;')
+} 
+
+function adjustCategoriesContainer() {
+	var containerWidth = $categories.width();
+	var contentWidth = $categories[0].scrollWidth;
+	if (containerWidth < contentWidth) {
+		$scrollButtons.removeClass('hidden');
+	} else {
+		$scrollButtons.addClass('hidden');
+	}
+}
+
+function scrollCategoriesContainer(direction) {
+	var offset = $('.category').first().width() * direction;
+	var scrollLeft = $categories.scrollLeft() + offset;
+	$categories.animat({ scrollLeft: scrollLeft }, 250);
+}
