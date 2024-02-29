c98a44a74387f7b0e91d18705bdd7482cb080cb3
unstoppable <internalminds01@gmail.com>
2024-02-29 10:26:14 +0800

    Project completed at this point

--- choose-color/main.js
+++ choose-color/main.js
@@ -1,5 +1,7 @@
 console.clear();
 
+var catId = 1;
+
 var THECATEGORY,
 	THEPROMPT,
 	RECIPIENT,
@@ -10,4 +12,33 @@ getPrompt();
 function getPrompt(category) {
 	$('body').removeClass(THECOLOR);
 	$('button').removeClass('active');
+
+	switch(category) {
+		case 'Writing':
+			catId = '2';
+			THECOLOR = category;
+			$('.' + category).addClass('active');
+			break;
+		case 'Drawing':
+			catId = '3';
+			THECOLOR = category;
+			$('.' + category).addClass('active');
+			break;
+		case 'Compliment':
+			catId = '4';
+			THECOLOR = category;
+			$('.' + category).addClass('active');
+			break;
+		case 'Quote':
+			catId = '5';
+			THECOLOR = category;
+			$('.' + category).addClass('active');
+			break;
+		default:
+			catId = '1';
+			THECOLOR = 'Random';
+			$('.Random').addClass('active');
+	}
+
+	$('body').addClass(THECOLOR);
 }
