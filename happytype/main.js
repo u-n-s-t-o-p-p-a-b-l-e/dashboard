a0698367993dc16683df28654123cee0673d50e6
unstoppable <internalminds01@gmail.com>
2024-03-01 13:33:46 +0800

    JS file almost done

--- happytype/main.js
+++ happytype/main.js
@@ -1,4 +1,4 @@
-const words = 'i am so happy with my life and this vibe will manifest happiness always forever and ever and nobody will take that away from me as this will be perpetual in my mind and in my heart';
+const words = 'i am so happy with my life and this vibe will manifest happiness always forever and ever and nobody will take that away from me as this will be perpetual in my mind and in my heart'.split(' ');
 const wordsCount = words.length;
 const gameTime = 30 * 1000;
 window.timer = null;
@@ -39,7 +39,7 @@ function getWpm() {
 	const lastTypeWord = document.querySelector('.word.current');
 	const lastTypedWordIndex = words.indexOf(lastTypeWord) + 1;
 	const typedWords = words.slice(0, lastTypedWordIndex);
-	const correctWords = typeWords.filter(word => {
+	const correctWords = typedWords.filter(word => {
 		const letters = [...word.children];
 		const incorrectLetters = letters.filter(letter => letter.className.includes('incorrect'));
 		const correctLetters = letters.filter(letter => letter.className.includes('correct'));
@@ -56,6 +56,7 @@ function gameOver() {
 
 document.getElementById('game').addEventListener('keyup', ev => {
 	const key = ev.key;
+	const currentLetter = document.querySelector('.letter.current');
 	const currentWord = document.querySelector('.word.current');
 	const expected = currentLetter?.innerHTML || ' ';
 	const isLetter = key.length === 1 && key !== ' ';
@@ -68,4 +69,94 @@ document.getElementById('game').addEventListener('keyup', ev => {
 	}
 
 	console.log({key,expected});
-})
+
+	if (!window.timer && isLetter) {
+		window.timer = setInterval(() => {
+			if (!window.gameStart) {
+				window.gameStart = (new Date()).getTime();
+			}
+			const currentTime = (new Date()).getTime();
+			const msPassed = currentTime - window.gameStart;
+			const sPassed = Math.round(msPassed / 1000);
+			const sLeft = Math.round((gameTime / 1000) - sPassed);
+			if (sLeft <= 0) {
+				gameOver();
+				return;
+			}
+			document.getElementById('info').innerHTML = sLeft + '';
+		}, 1000);
+	}
+
+	if (isLetter) {
+		if (currentLetter) {
+			addClass(currentLetter, key === expected ? 'correct' : 'incorrectLetters');
+			removeClass(currentLetter, 'current');
+			if (currentLetter.nextSibling) {
+				addClass(currentLetter.nextSibling, 'current');
+			}
+		} else {
+			const incorrectLetter = document.createElement('span');
+			incorrectLetter.innerHTML = key;
+			incorrectLetter.className = 'letter incorrect extra';
+			currentWord.appendChild(incorrectLetter);
+		}
+	}
+
+	if (isSpace) {
+		if (expected !== ' ') {
+			const lettersToInvalidate =[...document.querySelectorAll('.word.current .letter:not(.correct)')];
+			lettersToInvalidate.forEach(letter => {
+				addClass(letter, 'incorrect');
+			});
+		}
+		removeClass(currentWord, 'current');
+		addClass(currentWord.nextSibling, 'current');
+		if (currentLetter) {
+			removeClass(currentLetter, 'current');
+		}
+		addClass(currentWord.nextSibling.firstChild, 'current');
+	}
+
+	if (isBackspace) {
+		if (currentLetter && isFirstLetter) {
+			removeClass(currentWord, 'current');
+			addClass(currentWord.previousSibling, 'current');
+			removeClass(currentLetter, 'current');
+			addClass(currentWord.previousSibling.lastChild, 'current');
+			removeClass(currentWord.previousSibling.lastChild, 'incorrect');
+			removeClass(currentWord.previousSibling.lastChild, 'correct');
+		}
+
+		if (currentLetter && !isFirstLetter) {
+			removeClass(currentLetter, 'current');
+			addClass(currentLetter.previousSibling, 'current');
+			removeClass(currentLetter.previousSibling, 'incorrect');
+			removeClass(currentLetter.previousSibling, 'correct');
+		}
+
+		if (!currentLetter) {
+			addClass(currentWord.lastChild, 'current');
+			removeClass(currentWord.lastChild, 'incorrect');
+			removeClass(currentWord.lastChild, 'correct');
+		}
+	}
+
+	if (currentWord.getBoundingClientRect().top >  250) {
+		const words = document.getElementById('words');
+		const margin = parseInt(words.style.marginTop || '0px');
+		words.style.marginTop = (margin - 35) + 'px';
+	}
+
+	const nextLetter = document.querySelector('.letter.current');
+	const nextWord = document.querySelector('.word.current');
+	const cursor = document.getElementById('cursor');
+	cursor.style.top = (nextLetter || nextWord).getBoundingClientRect().top + 2 + 'px';
+	cursor.style.left = (nextLetter || nextWord).getBoundingClientRect()[nextLetter ? 'left' : 'right'] + 'px';
+});
+
+document.getElementById('newGameBtn').addEventListener('click', () => {
+	gameOver();
+	newGame();
+});
+
+newGame();
