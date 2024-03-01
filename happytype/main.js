d7e7da08bbbe831d7bef1aec1848d2a3d53660c0
unstoppable <internalminds01@gmail.com>
2024-03-01 15:00:20 +0800

    Need some fixing later on

--- happytype/main.js
+++ happytype/main.js
@@ -45,6 +45,7 @@ function getWpm() {
 		const correctLetters = letters.filter(letter => letter.className.includes('correct'));
 		return incorrectLetters.length === 0 && correctLetters.length === letters.length;
 	});
+	return correctWords.length / gameTime * 60000;
 }
 
 function gameOver() {
