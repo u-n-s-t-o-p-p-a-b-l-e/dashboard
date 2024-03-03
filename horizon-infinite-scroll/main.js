d0c09404fa0a9ac133be0df927c34c46bf5f4324
unstoppable <internalminds01@gmail.com>
2024-03-03 14:58:40 +0800

    JS finalized. Project completed at this point

--- horizon-infinite-scroll/main.js
+++ horizon-infinite-scroll/main.js
@@ -6,10 +6,15 @@ if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
 
 function addAnimation() {
 	scrollers.forEach((scroller) => {
+
 		scroller.setAttribute("data-animated", true);
+
 
 		const scrollerInner = scroller.querySelector(".scroller-inner");
 		const scrollerContent = Array.from(scrollerInner.children);
+
+
+
 
 		scrollerContent.forEach((item) => {
 			const duplicatedItem = item.cloneNode(true);
