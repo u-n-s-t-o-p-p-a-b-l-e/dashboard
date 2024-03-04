62baa13bfdeaa810937485f820bb27db509da36b
unstoppable <internalminds01@gmail.com>
2024-03-05 02:01:31 +0800

    Revert using ext Tailwind while debugging missing part

--- realtime-filter/main.js
+++ realtime-filter/main.js
@@ -6,87 +6,77 @@ const products = [
 		category: 'games',
 		price: 499.99,
 	},
-{
+	{
 
 		name: 'Samsung Galaxy',
 		url: './img/samsung-galaxy.png',
 		category: 'smartphones',
 		price: 399.99,
 	},
-
-{
+	{
 
 		name: 'Cannon EOS Camera',
 		url: './img/cannon-eos-camera.png',
 		category: 'games',
 		price: 749.99,
 	},
-
-{
+	{
 
 		name: 'Sony A7 Camera',
 		url: './img/sony-a7-camera.png',
 		category: 'cameras',
 		price: 1999.99,
 	},
-
-{
+	{
 
 		name: 'LG TV',
 		url: './img/lg-tv.png',
 		category: 'televisions',
 		price: 799.99,
 	},
-
-{
+	{
 
 		name: 'Nintendo Switch',
 		url: './img/nintendo-switch.png',
 		category: 'games',
 		price: 299.99,
 	},
-
-{
+	{
 
 		name: 'Xbox Series X',
 		url: './img/xbox-series-x.png',
 		category: 'games',
 		price: 499.99,
 	},
-
-{
+	{
 
 		name: 'Samsung TV',
 		url: './img/samsung-tv.png',
 		category: 'televisions',
 		price: 1099.99,
 	},
-
-{
+	{
 
 		name: 'Sony ZV1F Camera',
 		url: './img/sony-zv1f-camera.png',
 		category: 'cameras',
 		price: 799.99,
 	},
-
-{
+	{
 
 		name: 'Google Pixel',
 		url: './img/google-pixel.png',
 		category: 'smartphones',
 		price: 499.99,
 	},
-
-{
+	{
 
 		name: 'Toshiba TV',
 		url: './img/toshiba-tv.png',
 		category: 'televisions',
 		price: 499.99,
 	},
-
-{
+	{
 
 		name: 'iPhone 14',
 		url: './img/iphone-14.png',
@@ -120,16 +110,9 @@ function createProductElement(product) {
 
 	productElement.className = 'item space-y-2';
 
-	productElement.innerHTML = `<div class="bg-gray-100 flex justify-center relative overflow-hidden group cursor-pointer border rounded-xl"
->
-<img
-src="${product.url}"
-alt="${product.name}"
-class="w-full h-full object-cover"
-/>
-<button class="status bg-black text-white absolute bottom-0 left-0 right-0 text-center py-2 translate-y-full transition group-hover:translate-y-0"
->Add To Cart</button>
-</div>
+	productElement.innerHTML = `<div class="bg-gray-100 flex justify-center relative overflow-hidden group cursor-pointer border rounded-xl" >
+<img src="${product.url}" alt="${product.name}" class="w-full h-full object-cover" />
+<button class="status bg-black text-white absolute bottom-0 left-0 right-0 text-center py-2 translate-y-full transition group-hover:translate-y-0" >Add To Cart</button> </div>
 <p class="text-xl">${product.name}</p>
 <strong>$${product.price.toLocaleString()}</strong>`;
 
@@ -140,7 +123,7 @@ class="w-full h-full object-cover"
 
 function updateCart(e) {
 	const statusEl = e.target; 
-	
+
 	if (statusEl.classList.contains('added')) {
 		statusEl.classList.remove('added');
 		statusEl.innerText = 'Add To Cart';
