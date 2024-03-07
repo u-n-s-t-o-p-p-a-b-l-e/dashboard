5525af69b4949e1e462a8d9d5d46f1f07e526c01
unstoppable <internalminds01@gmail.com>
2024-03-07 16:18:49 +0800

    Added JS code

---
+++ more-typing/main.js
@@ -0,0 +1,6 @@
+const sourceText = document.getElementById('sourceText');
+const lineDisplay = document.getElementById('lineDisplay');
+const typingArea = document.getElementById('typingArea');
+const wpmDisplay = document.getElementById('wpmDisplay');
+const accuracyDisplay = document.getElementById('accuracyDisplay');
+const fileInput = document.getElementById('fileInput');
