8bbe88738c0be171e958ec37e75b0f93f772e663
unstoppable <internalminds01@gmail.com>
2024-03-08 19:05:17 +0800

    Added JS code

---
+++ mobile-global-registration/main.js
@@ -0,0 +1,10 @@
+const countries = [
+
+
+], 
+	select_box = document.querySelector(".options"),
+	search_box = document.querySelector(".search-box"),
+	input_box = document.querySelector('input[type="tel"]'),
+	selected_option = document.querySelector(".selected-option div");
+
+let options = null;  
