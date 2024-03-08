f411960946a0142ce5ef52c52c6cd25c4c6c1f4e
unstoppable <internalminds01@gmail.com>
2024-03-08 19:36:07 +0800

    JS updated to finalize

--- mobile-global-registration/main.js
+++ mobile-global-registration/main.js
@@ -108,7 +108,6 @@ const countries = [
 		{ name: "Iraq", code: "IQ", phone: 964 },
 		{ name: "Ireland", code: "IE", phone: 353 },
 		{ name: "Isle of Man", code: "IM", phone: 44 },
-		{ name: "Israel", code: "IL", phone: 972 },
 		{ name: "Italy", code: "IT", phone: 39 },
 		{ name: "Jamaica", code: "JM", phone: 1876 },
 		{ name: "Japan", code: "JP", phone: 81 },
@@ -268,3 +267,50 @@ const countries = [
 	selected_option = document.querySelector(".selected-option div");
 
 let options = null;  
+
+for (country of countries) {
+	const option = `
+	<li class="option">
+	<div>
+	<span class="iconify" data-icon="flag:${country.code.toLowerCase()}-4x3"></span>
+	<span class="country-name">${country.name}</span>
+	</div>
+	<strong>+${country.phone}</strong>
+	</li>`;
+
+	select_box.querySelector("ol").insertAdjacentHTML("beforeend", option);
+	options = document.querySelectorAll(".option");
+}
+
+function selectOption() {
+	console.log(this);
+	const icon = this.querySelector(".iconify").cloneNode(true),
+		phone_code = this.querySelector("strong").cloneNode(true);
+
+	selected_option.innerHTML = "";
+	selected_option.append(icon, phone_code);
+
+	input_box.value = phone_code.innerText;
+
+	select_box.classList.remove("active");
+	selected_option.classList.remove("active");
+
+	search_box.value = "";
+	select_box.querySelectorAll(".hide").forEach((el) => el.classsList.remove("hide"));
+}
+
+function searchCountry() {
+	let search_query = search_box.value.toLowerCase();
+	for (option of options) {
+		let is_matched = option.querySelector(".country-name").innerText.tolowerCase().includes(search_query);
+			option.classList.toggle("hide", !is_matched);
+	}
+}
+
+selected_option.addEventListener("click", () => {
+	select_box.classList.toggle("active");
+	selected_option.classList.toggle("active");
+});
+
+options.forEach((option) => option.addEventListener("click", selectOption));
+search_box.addEventListener("input", searchCountry); 
