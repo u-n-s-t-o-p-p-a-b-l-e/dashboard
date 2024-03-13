2d73f25f03f974b785154925072ac7a6cd3304d8
unstoppable <internalminds01@gmail.com>
2024-03-13 21:38:19 +0800

    Bugs fixed, completed at this point

--- matrix-screensaver/main.js
+++ matrix-screensaver/main.js
@@ -21,7 +21,7 @@ function draw()
 	ctx.fillStyle = "rgba(0, 0, 20, 0.05)";
 	ctx.fillRect(0, 0, c.width, c.height);
 	let rn = Math.floor(Math.random() * 255);
-	ctx.fillStyle = "rgba(0, "+ rn+20 +" , " + rn+90 + ")";
+	ctx.fillStyle = "rgba(0,"+ rn+20 +"," + rn+90 + ")";
 	ctx.font = font_size + "px sans-serif";
 	for(var i = 0; i < drops.length; i++)
 	{
@@ -64,13 +64,14 @@ init = () => {
 
 doc.on("mousemove", (e) => {
 	mX = e.pageX
-	mY = e,pageY
-	letter.push([mX-20+mT(20, 0), mY+mR(20, 0), letters[mR(letters.length, 0)], mR(10, 8), 1, mR(6, 1), mR(20, 0)])
+	mY = e.pageY
+	letter.push([mX-10+mR(20, 0), mY+mR(20, 0), letters[mR(letters.length, 0)], mR(10, 8), 1, mR(6, 1), mR(20, 0)])
 })
 
 dT = (x, y, t, s, c) => {
 	con.save()
 	con.font = ""+s+"px Lucida Console"
+	con.shadowColor = "rgba(0, 180, 255, "+c+")"
 	con.shadowBlur = s/2
 	con.fillStyle = "rgba(0, 200, 140, "+c+")"
 	let tW = con.measureText(t).width
@@ -82,7 +83,6 @@ dR = (x, y, w, h, c) => {
 	con.save()
 	con.beginPath()
 	con.rect(x, y, w, h)
-	con.rect(x, y, w, h)
 	con.fillStyle = c
 	con.fill()
 	con.restore()
@@ -91,9 +91,9 @@ dR = (x, y, w, h, c) => {
 think = () => {
 	let sC = mR(2, 1)
 	for (let i = 0; i < letter.length; i++) {
-		sC == 2 ? letter ? letter[i][2] = letters[mR(letters.length, 0)]:
-		letter ? letter[i][1]-= letter[i][5]:
-		letter[i][4] >= 0 ? letter[i][4] -= 0.01: letter.splice(i, 1)
+		sC == 2 ? letter ? letter[i][2] = letters[mR(letters.length, 0)]: null:null
+		letter ? letter[i][1]-= letter[i][5]: null
+		letter[i][4] >= 0 ? letter[i][4]-= 0.01: letter.splice(i, 1)
 	}
 	animate()
 	window.requestAnimationFrame(think)
@@ -102,10 +102,10 @@ think = () => {
 animate = () => {
 	con.clearRect(0, 0, can.width, can.height)
 	for (let i = 0; i < letter.length; i++) {
-		dT(letter[i][0], letter[i][1], letter[i][2], letter[i][3], letter[i][4]) 
+		dT(letter[i][0], letter[i][1], letter[i][2], letter[i][3], letter[i][4])
 			let rH = mR(540, 10)
 			let sH = mR(rH, 1)
-			letter[i][6] == 2 ? dR(letter[i][0], letter[i][1]-sH, letter[i][3]/1.5, rH, "rgba(0, 255, 100, 0.05)")
+			letter[i][6] == 2 ? dR(letter[i][0], letter[i][1]-sH, letter[i][3]/1.5, rH, "rgba(0, 255, 100, 0.05)"): null
 		
 	}
 }
