c3e2f29ebc7b537ee9848def9ff290ee713378db
unstoppable <internalminds01@gmail.com>
2024-03-21 22:07:31 +0800

    JS code Done

--- toggles-animation/script.js
+++ toggles-animation/script.js
@@ -31,3 +31,46 @@ SVG.on(document, "DOMContentLoaded", () => {
 	new SvgToggleEffect(randomSlide);
 	new SvgToggleEffect(delayedOffsetSlide);
 });
+
+class SvgToggleEffect {
+	constructor(effect) {
+		this.nodes = [
+			...effect.element.nextElementSibling.querySelectorAll(".row")
+		];
+
+		this.nodes.forEach((node, index) => {
+			this.nodes[index] = SVG(node);
+		});
+
+		if (effect.random) {
+			this.randomizeArray(this.nodes);
+		}
+
+		effect.element.addEventListener("change", () => {
+			if (effect.element.checked) {
+				effect.handler(this.nodes, false, effect.duration, effect.offset);
+			} else {
+				effect.handler(this.nodes, true, effect.duration, effect.offset);
+			}
+		});
+	}
+
+	randomizeArray(array) {
+		for (let i = array.length -1; i > 0; i--) {
+			const j = Math.floor(Math.random() * (i + 1));
+			[array[i], array[j]] = [array[j], array[i]];
+		}
+	}
+}
+
+const svgSlideEffect = (nodes = [], reverse = false, duration = 100, offset = 10) => {
+	nodes.forEach((node, index) => {
+		setTimeout(() => {
+			if (reverse) {
+				node.animate({ duration: duration }).transform({ translate: 0 });
+			} else {
+				node.animate({ duration: duration }).transform({ translate: 48  });
+			}
+		}, index * offset);
+	});
+};
